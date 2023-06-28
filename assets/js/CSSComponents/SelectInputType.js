9f9f911bfcc049a2d85bb2d03512253ca4b763a2
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-06-28 08:31:33 -0300

    feat: working input bars

--- assets/js/CSSComponents/SelectInputType.js
+++ assets/js/CSSComponents/SelectInputType.js
@@ -1,16 +1,21 @@
 window.addEventListener('load', function () {
-  console.log('SelectInputType.js loaded')
   const selectInputManual = document.querySelector('#select-input-manual')
   const selectInputRandom = document.querySelector('#select-input-random')
+  const manualInputBar = document.querySelector('#manual-container')
+  const randomInputBar = document.querySelector('#random-container')
 
   selectInputManual.addEventListener('click', function () {
     console.log('selectInputManual')
     selectInputManual.classList.add('selected')
     selectInputRandom.classList.remove('selected')
+    manualInputBar.style.display = 'block'
+    randomInputBar.style.display = 'none'
   })
 
   selectInputRandom.addEventListener('click', function () {
     selectInputRandom.classList.add('selected')
     selectInputManual.classList.remove('selected')
+    randomInputBar.style.display = 'block'
+    manualInputBar.style.display = 'none'
   })
 })

bb783a0f106fabd2c6666afd70fe870f365163af
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-06-27 23:29:41 -0300

    feat: fanout selector

--- assets/js/Controlls.js
+++ assets/js/Controlls.js
@@ -1,18 +1,37 @@
 class Controlls extends Observable {
   constructor() {
     super()
+    this.createTree()
     this.init()
     this.addButtonsEventListeners()
+    this.fanout = 4
+  }
+
+  createTree() {
+    this.tree = new BPlusTree(4)
+    this.treeVisualizer = new BPlusTreeVisualizer(this.tree)
+  }
+
+  createNewTree() {
+    this.treeVisualizer.clear()
+    this.tree = new BPlusTree(this.fanout)
+    this.treeVisualizer = new BPlusTreeVisualizer(this.tree)
   }
 
   init() {
-    this.tree = new BPlusTree(4)
-    this.treeVisualizer = new BPlusTreeVisualizer(this.tree)
-
     this.manualInputKey = document.getElementById('manual-input-key')
     this.manualInsertButton = document.getElementById('manual-insert-button')
     this.manualSearchButton = document.getElementById('manual-search-button')
     this.manualDeleteButton = document.getElementById('manual-delete-button')
+    this.showFanout = document.getElementById('show-fanout')
+    this.showFanout.textContent = this.tree.fanout
+
+    this.descreaseFanoutButton = document.getElementById(
+      'decrease-fanout-button',
+    )
+    this.increaseFanoutButton = document.getElementById(
+      'increase-fanout-button',
+    )
   }
 
   addButtonsEventListeners() {
@@ -35,6 +54,24 @@ class Controlls extends Observable {
 
       const value = this.manualInputKey.value
       this.handleManualAction({ action: 'delete', value })
+    })
+
+    this.increaseFanoutButton.addEventListener('click', e => {
+      e.preventDefault()
+
+      if (this.tree.fanout === 10) return
+      this.fanout = this.fanout + 1
+      this.showFanout.textContent = this.fanout
+      this.createNewTree()
+    })
+
+    this.descreaseFanoutButton.addEventListener('click', e => {
+      e.preventDefault()
+
+      if (this.tree.fanout === 3) return
+      this.fanout = this.fanout - 1
+      this.showFanout.textContent = this.fanout
+      this.createNewTree()
     })
   }
 
